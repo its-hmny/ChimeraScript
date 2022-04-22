67438f4208b0b91f047500a8de500cb1c3d43927
Enea Guidi <guidienea@gmail.com>
2022-04-22 23:46:29 +0200

    test(GutPuller): Completed test suite for GitPuller script

--- tests/test_gitpuller.py
+++ tests/test_gitpuller.py
@@ -1,8 +1,11 @@
-from os.path import isdir, join, exists
-from random import random
-from tempfile import NamedTemporaryFile
+"""PyTest module with test suite implementation for the GitPuller.py script"""
+from os import listdir, system
+from os.path import exists, isdir, join
+from random import choice, randint, random
+from tempfile import NamedTemporaryFile, mkdtemp
 
 from pytest import raises
+from requests import get
 from scripts.GitPuller import fetch_repos, fetch_stars
 
 
@@ -42,10 +45,47 @@ class TestGitPuller:
         assert exists(tmp_file_path) is True, "(stars): Overritten the filepath"
         assert isdir(tmp_file_path) is False, "(stars): Changed the filepath to a directory"
 
-    def test_clone_new_flag(self):
-        """Checks that the "clone_new" flags works correctly"""
-        assert False, "Test case not completed"
+    def test_repos_clone_new_flag(self):
+        """Checks that the "clone_new" flags works correctly for repo subcommand"""
+        # Creates a temporary directory
+        tmp_dir = mkdtemp()
+        # Uses GitHub API to get a list of all my public repositories
+        res = get("https://api.github.com/search/repositories?q=user:its-hmny")
+        repo_list = res.json().get("items", [])
 
-    def test_ignore_archived(self):
+        # Clones a random number of repos manually
+        for _ in range(randint(0, len(repo_list))):
+            to_be_cloned = choice(repo_list)
+            system(f"cd {tmp_dir} && git clone {to_be_cloned['clone_url']}")
+
+        # Executes the script on the temp directory to tests that the missing ones are cloned
+        fetch_repos(tmp_dir, clone_new=True)
+        assert len(listdir(tmp_dir)) == len(repo_list), "Not all the repos have been cloned"
+
+    def test_stars_clone_new_flag(self):
+        """Checks that the "clone_new" flags works correctly for stars subcommand"""
+        # Creates a temporary directory
+        tmp_dir = mkdtemp()
+        # Uses GitHub API to get a list of all my public repositories
+        stars_list = get("https://api.github.com/users/its-hmny/starred").json()
+
+        # Clones a random number of repos manually
+        for _ in range(randint(0, len(stars_list))):
+            to_be_cloned = choice(stars_list)
+            system(f"cd {tmp_dir} && git clone {to_be_cloned['clone_url']}")
+
+        # Executes the script on the temp directory to tests that the missing ones are cloned
+        fetch_stars(tmp_dir, clone_new=True)
+        assert len(listdir(tmp_dir)) == len(stars_list), "Not all the repos have been cloned"
+
+    def test_repos_ignore_archived_flags(self):
         """Checks that the "ignore_archived" flags works correctly"""
-        assert False, "Test case not completed"
+        # Creates a temporary directory
+        tmp_dir = mkdtemp()
+        # Uses GitHub API to get a list of all my public repositories
+        res = get("https://api.github.com/search/repositories?q=user:its-hmny")
+        not_archived = [repo for repo in res.json().get("items", []) if not repo["archived"]]
+
+        # Executes the script on the temp directory and tests that only the not archived are cloned
+        fetch_repos(tmp_dir, clone_new=True, ignore_archived=True)
+        assert len(listdir(tmp_dir)) == len(not_archived), "Some archived repos have been cloned"
